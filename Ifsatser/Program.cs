a6abaf4b88e4a9e6904764fe1ab5555271f36d2e
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-03 10:36:48 +0200

    Initial commit

---
+++ Ifsatser/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ifsatser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name;
+
+      name = Console.ReadLine();
+
+      //Console.WriteLine("micke" != "herbert");
+
+      // Kolla ifall användaren skrev "Micke"
+      if (name == "micke")
+      {
+        Console.WriteLine("Hej!");
+
+        Console.WriteLine("Vad ska du göra idag?");
+
+        string todo = Console.ReadLine();
+
+        if (todo == "sleep")
+        {
+          Console.WriteLine("Zzzzz");
+        }
+
+      }
+      else if (name == "herbert")
+      {
+        Console.WriteLine("Tjena!");
+      }
+      else
+      {
+        Console.WriteLine("Nej!");
+      }
+
+      // if (name != "micke")
+      // {
+      // }
+
+
+      Console.ReadLine();
+
+    }
+  }
+}
